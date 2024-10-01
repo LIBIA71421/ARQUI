023e937c042c3093a1ee0427571e47abdb9b8a20
libia <libia.roman@ucb.edu.bo>
2024-10-01 19:18:07 -0400

    Start

---
+++ Translate-Service/src/test/java/Translate/Service/TranslateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Translate.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
